2d6a67f3479214074d99142375d3df816ece7aed
Natalya Babenko <natapervomaisk88@gmail.com>
2024-09-06 15:36:34 +0300

    added fetch

--- less2/index.js
+++ less2/index.js
@@ -1,4 +1,4 @@
-// import "dotenv/config";
+import "dotenv/config";
 // import dotenv from "dotenv";
 // dotenv.config();
 // console.log(process.env.APP_TITLE, process.env.PORT);
@@ -25,16 +25,60 @@
 //   console.log("microtask");
 // });
 
-const product = {
-  id: 1,
-  title: "tv",
-  price: 300000,
-};
+// const product = {
+//   id: 1,
+//   title: "tv",
+//   price: 300000,
+// };
 
 // const new_product = { ...product };
 // product.id = 100;
 // console.table(new_product);
-const new_product = structuredClone(product);
-product.id = 100;
-console.table(new_product);
+// const new_product = structuredClone(product);
+// product.id = 100;
+// console.table(new_product);
 //const product2 = product;
+
+// const user = {
+//   id: 1,
+//   name: "Oleg",
+//   address: {
+//     street: "Sadova",
+//     ap: 20,
+//   },
+// };
+
+// const new_user = structuredClone(user);
+// user.address.ap = 133;
+// console.table(new_user);
+
+// const new_user = { ...user };
+// user.address.ap = 133;
+// console.table(new_user);
+
+// const str = "Node JS";
+// const encoding = btoa(str); //кодування base64
+// console.log(encoding);
+// const decoding = atob(encoding); //декодування base64
+// console.log(decoding);
+// const start = performance.now();
+// let s = 0;
+// for (let i = 0; i < 1e10; i++) {
+//   s += i;
+// }
+// console.log(s);
+// console.log(`Time: ${performance.now() - start} ms`);
+
+// fetch(`${process.env.HOST}users`)
+//   .then((res) => res.json())
+//   .then((data) => console.log(data))
+//   .catch((e) => console.log(e));
+
+fetch(`${process.env.HOST}users`, {
+  method: "POST",
+  body: JSON.stringify({
+    name: "Ruslan",
+  }),
+})
+  .then((res) => console.log(res))
+  .catch((e) => console.log(e));
